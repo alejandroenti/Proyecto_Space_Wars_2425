6e5019d9acc2d743ecc4551a4b6fe8878bedb460
Alejandro López <116338818+alejandroenti@users.noreply.github.com>
2025-05-05 18:28:33 +0200

    Creo la clase Printing
    En ella pondré diferentes métodos para poder imprimir Strings de manera formateada

---
+++ M3/src/utils/Printing.java
@@ -0,0 +1,16 @@
+package utils;
+
+public class Printing {
+
+	public static String printTitle(String str) {
+		return String.format("%s\n\n", str);
+	}
+	
+	public static String printStringSized(String str, int numChars) {
+		return String.format("%"+numChars+"s", str);
+	}
+	
+	public static String printNumberSized(int number, int numChars) {
+		return String.format("%-"+numChars+"d", number);
+	}
+}
