539da4dd573c9c7374923605470d03efa64b9b02
monica438 <monica14dominguez@gmail.com>
2025-05-06 18:10:29 +0200

    new EnemyPanel and PlayerPanel

---
+++ M3/src/gui/PlayerPanel.java
@@ -0,0 +1,15 @@
+package gui;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class PlayerPanel extends JPanel{
+	PlayerPanel(int width, int height){
+		
+		setSize(width,height);
+		setBackground(Color.RED);
+		setLocation(0,0);
+		
+	}
+}
