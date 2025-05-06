ffeb370b845d718b2793b21e1dde7edafcda964c
monica438 <monica14dominguez@gmail.com>
2025-05-06 19:01:13 +0200

    new ButtonsPanel

---
+++ M3/src/gui/ButtonsPanel.java
@@ -0,0 +1,13 @@
+package gui;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class ButtonsPanel extends JPanel{
+	
+	ButtonsPanel(){
+		add(new JButton("a"));
+		add(new JButton("b"));
+		add(new JButton("c"));
+	}
+}
