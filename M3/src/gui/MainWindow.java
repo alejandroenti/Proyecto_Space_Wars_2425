cc0bb87844e1b0006772b2c10a1f24d027ec173e
Alejandro López <116338818+alejandroenti@users.noreply.github.com>
2025-05-06 16:54:34 +0200

    Segunda prueba de ventana

--- M3/src/gui/MainWindow.java
+++ M3/src/gui/MainWindow.java
@@ -7,17 +7,15 @@ import javax.swing.JFrame;
 
 public class MainWindow extends JFrame{
 	
-	private final int[] INIT_POS = { -10, 0 };
+	private final int WIDTH = 1200, HEIGHT = 675;
 	
 	public MainWindow() {
 		
-		Toolkit pantalla = Toolkit.getDefaultToolkit();
-		Dimension size = pantalla.getScreenSize();
-		
 		this.setTitle("Space Wars");
+		this.setSize(WIDTH, HEIGHT);
+		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setResizable(false);
-		this.setBounds(INIT_POS[0], INIT_POS[1], (int)(size.getWidth() * 10), (int)(size.getHeight() * 10));
 		
 		this.setVisible(true);
 	}
