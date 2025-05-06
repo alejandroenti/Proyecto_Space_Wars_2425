de37ac32866c56da2345ac7aa260d26118b4b1f9
Alejandro López <116338818+alejandroenti@users.noreply.github.com>
2025-05-06 18:37:27 +0200

    Moving enemyPanel when is approaching

--- M3/src/gui/MainWindow.java
+++ M3/src/gui/MainWindow.java
@@ -1,24 +1,89 @@
 package gui;
 
-import java.awt.Dimension;
-import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class MainWindow extends JFrame{
+public class MainWindow extends JFrame implements VariablesWindow {
 	
-	private final int WIDTH = 1200, HEIGHT = 675;
+	private final String FRAME_TITLE = "Space Wars";
+	private final String URL_LOGO = "./src/art/logo.png";
+	
+	private JPanel mainPanel;
+	private EnemyPanel enemyPanel;
+	private BufferedImage appLogo;
 	
 	public MainWindow() {
 		
-		this.setTitle("Space Wars");
-		this.setSize(WIDTH, HEIGHT);
-		this.setLocationRelativeTo(null);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setResizable(false);
+		setupFrame();
+		initMainPanel();
+		
+		approachEnemy();
 		
 		this.setVisible(true);
 	}
 	
+	private void setupFrame() {
+		
+		this.setTitle(FRAME_TITLE);
+		this.setSize(WIDTH, HEIGHT);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setResizable(false);
+		loadIconImage();
+	}
 	
+	private void loadIconImage() {
+		
+		try {
+            appLogo = ImageIO.read(new File(URL_LOGO));
+            this.setIconImage(appLogo);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+	
+	private void initMainPanel() {
+		
+		mainPanel = new JPanel();
+		mainPanel.setLayout(null); // Hacemos el Layout completamente flexible y posicionamos de manera absoluta en cuanto a la Ventana
+
+		
+		/*
+		 * 1. Inicializar Panel del Jugador
+		 * 2. Inicializar Panel del Enemigo
+		 * 3. Llevar el Panel del Jugador a la posición 0,0
+		 * 4. Llevar el Panel del Enemigo a Parla
+		 * 5. Inicializar Panel de Opciones
+		 * 6. Llevar el Panel de Opciones al Centro Abajo de la Ventana 
+		 */
+		
+		enemyPanel = new EnemyPanel((int)(WIDTH / 2), HEIGHT);
+		
+		mainPanel.add(enemyPanel);
+
+		this.add(mainPanel);
+	}
+	
+	public void approachEnemy() {
+		
+		Timer timer = new Timer();
+		TimerTask task = new TimerTask() {
+			public void run() {
+			 enemyPanel.enemyComing();
+			 
+			 if (enemyPanel.getPosX() <= (int)(WIDTH / 2)) {
+				 timer.cancel();
+			 }
+			}
+		};
+		
+		timer.schedule(task, DELAY, (int)(TIME / STEPS));
+	}
 }
