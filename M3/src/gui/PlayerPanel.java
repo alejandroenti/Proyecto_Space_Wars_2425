95a7934bec2fb0c5cf385deaa0dae525d5df0574
Alejandro López <116338818+alejandroenti@users.noreply.github.com>
2025-05-06 19:56:34 +0200

    Adding constructor of super class

--- M3/src/gui/PlayerPanel.java
+++ M3/src/gui/PlayerPanel.java
@@ -7,7 +7,9 @@ import javax.swing.JPanel;
 import utils.VariablesWindow;
 
 public class PlayerPanel extends JPanel implements VariablesWindow {
-	PlayerPanel(){
+	
+	public PlayerPanel(){
+		super();
 		
 		setSize((int)(FRAME_WIDTH / 2), FRAME_HEIGHT);
 		setBackground(Color.RED);
