6399c5d493948684424f75afb4434f6c7a412f54
monica438 <monica14dominguez@gmail.com>
2025-05-06 19:09:24 +0200

    Update ButtonsPanel.java

--- M3/src/gui/ButtonsPanel.java
+++ M3/src/gui/ButtonsPanel.java
@@ -1,13 +1,25 @@
 package gui;
 
+import java.awt.Color;
+
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class ButtonsPanel extends JPanel{
+	JButton buy, update, reports;
 	
 	ButtonsPanel(){
-		add(new JButton("a"));
-		add(new JButton("b"));
-		add(new JButton("c"));
+		setLayout(new BoxLayout(this,BoxLayout.X_AXIS));
+		setLocation(0,0);
+		setBackground(Color.BLUE);
+		
+		buy = new JButton("a");
+		update = new JButton("b");
+		reports = new JButton("c");
+		
+		add(buy);
+		add(update);
+		add(reports);
 	}
 }
