2608d5cb34bb320ec90d67c4b266fb527033b7c6
Alejandro López <116338818+alejandroenti@users.noreply.github.com>
2025-05-07 18:02:54 +0200

    Rotating BufferedImage from a given rotation in degrees

--- M3/src/gui/ImagePanel.java
+++ M3/src/gui/ImagePanel.java
@@ -2,7 +2,9 @@ package gui;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -13,11 +15,36 @@ import javax.swing.JPanel;
 public class ImagePanel extends JPanel {
 
 	private BufferedImage image;
+	private boolean rotate;
 	
 	public ImagePanel(String urlImage) {
 		super();
 		setOpaque(false);
 		loadImage(urlImage);
+		this.rotate = false;
+	}
+	
+	public void setRotate(boolean rotate) {
+		this.rotate = rotate;
+	}
+	
+	public void rotateImage(double theta) {
+		
+		if (image == null) return;
+		
+		// https://stackoverflow.com/questions/37758061/rotate-a-buffered-image-in-java
+		double rads = Math.toRadians(theta);
+		
+		System.out.println(image.getType());
+		BufferedImage rotatedImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_4BYTE_ABGR);
+		Graphics2D g2d = rotatedImage.createGraphics();
+		AffineTransform at = new AffineTransform();
+		at.rotate(rads, image.getWidth() / 2, image.getHeight() / 2);
+		g2d.setTransform(at);
+		g2d.drawImage(image, 0, 0, this);
+		g2d.dispose();
+		
+		image = rotatedImage;
 	}
 	
 	private void loadImage(String urlImage) {
