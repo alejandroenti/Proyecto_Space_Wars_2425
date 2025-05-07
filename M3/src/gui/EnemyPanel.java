2813c680f877cbbc307d7838ca8f37015f8d974d
Alejandro López <116338818+alejandroenti@users.noreply.github.com>
2025-05-07 19:58:49 +0200

    Manual positioning of enemy army

--- M3/src/gui/EnemyPanel.java
+++ M3/src/gui/EnemyPanel.java
@@ -1,5 +1,8 @@
 package gui;
 
+import java.awt.Color;
+import java.util.ArrayList;
+
 import javax.swing.JPanel;
 
 import utils.VariablesWindow;
@@ -11,13 +14,25 @@ public class EnemyPanel extends JPanel implements VariablesWindow {
 	private int posX;
 	private int approaching_speed;
 	
+	private ImagePanel planetPanel, armyPanel;
+	private int[] playerArmy;
+	private ArrayList<ImagePanel> armyPanels;
+	
 	EnemyPanel(){
 		approaching_speed = (INIT_POS_X - (int)(FRAME_WIDTH / 2)) / APPROACH_STEPS;
 		posX = INIT_POS_X;
 		
+		setLayout(null);
 		setSize((int)(FRAME_WIDTH / 2), FRAME_HEIGHT);
 		setOpaque(false);
-		setLocation(posX,POS_Y);
+		setLocation(posX, POS_Y);
+		
+		int num = (int)(1+ Math.random() * 9);
+		planetPanel = new ImagePanel(BASE_URL + "planet0" + num + ".png");
+		planetPanel.setBounds(getWidth() / 2, 0, (int)(FRAME_WIDTH / 2), FRAME_HEIGHT);
+				
+		add(planetPanel);
+		paint();
 		
 	}
 
@@ -50,5 +65,27 @@ public class EnemyPanel extends JPanel implements VariablesWindow {
 		setLocation(posX,POS_Y);
 	}
 	
+	private void paint() {
+		ImagePanel s1 = new ImagePanel(BASE_URL + "ships_0_1.png");
+		s1.setBounds(140, 337 - 64 - 50, 64, 64);
+		s1.rotateImage(90);
+		
+		ImagePanel s2 = new ImagePanel(BASE_URL + "ships_1_1.png");
+		s2.setBounds(140, 337 + 64 - 50, 64, 64);
+		s2.rotateImage(90);
+		
+		ImagePanel s3 = new ImagePanel(BASE_URL + "ships_2_1.png");
+		s3.setBounds(210, 337 - 64 * 3 - 50 * 2, 64, 64);
+		s3.rotateImage(90);
+		
+		ImagePanel s4 = new ImagePanel(BASE_URL + "ships_3_1.png");
+		s4.setBounds(210, 337 + 64 * 3 + 10,  64, 64);
+		s4.rotateImage(90);
+		
+		add(s1);
+		add(s2);
+		add(s3);
+		add(s4);
+	}
 	
 }
