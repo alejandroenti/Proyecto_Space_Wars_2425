d7b56656e109a1c107072b85541c2eef73c75346
monica438 <monica14dominguez@gmail.com>
2025-05-08 16:38:16 +0200

    Update printing methods

--- M3/src/utils/Printing.java
+++ M3/src/utils/Printing.java
@@ -13,4 +13,27 @@ public class Printing {
 	public static String printNumberSized(int number, int numChars) {
 		return String.format("%"+numChars+"d", number);
 	}
+	
+	public static String printStringCentred(String str, char character, int numChars) {
+		int length_str = str.length();
+		int chars_each_side = (int) ((numChars - length_str) / 2);
+		
+		String final_str = "";
+		
+		for (int i = 0; i < chars_each_side; i++) {
+			final_str += character;
+		}
+		
+		final_str += str;
+		
+		for (int i = 0; i < chars_each_side; i++) {
+			final_str += character;
+		}
+		
+		if (final_str.length() != numChars) {
+			final_str += character;
+		}
+		
+		return final_str;
+	}
 }
