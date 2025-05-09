18ebd120f676e86381c45bfc8dd3d87d415a8e62
Alejandro López <116338818+alejandroenti@users.noreply.github.com>
2025-05-09 16:46:08 +0200

    Changing approach time and adding Player Ships constants

--- M3/src/utils/VariablesWindow.java
+++ M3/src/utils/VariablesWindow.java
@@ -10,8 +10,26 @@ public interface VariablesWindow {
 	public final int FRAME_HEIGHT = 675;
 	
 	// timer parameters
-	public final int APPROACH_TIME = 1800;
+	public final int APPROACH_TIME = 180000;
 	public final int APPROACH_STEPS = 900;
 	public final int APPROACH_DELAY = 0;
 	
+	// Ships Initial Rotation
+	public final int INITAL_PLAYER_SHIP_ROTATION = 270;
+	public final int INITAL_ENEMY_SHIP_ROTATION = 90;
+	
+	// Player Ships Positions
+	public final int[][] PLAYER_SHIPS_POSITIONS = {
+			{ 400, 223 },
+			{ 400, 351 },
+			{ 330, 45 },
+			{ 330, 539 },
+			{ 165, 100 },
+			{ 220, 257 },
+			{ 180, 417 }
+	};
+	
+	public final int[] PLAYER_SHIPS_SIZES = {
+			64, 64, 64, 64, 96, 128, 128
+	};
 }
